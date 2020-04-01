41fecfa46d4acf3079f2734649f993d4c0971130
rohitworld90 <42111985+rohitworld90@users.noreply.github.com>
2020-04-01 16:39:21 +0300

    Change message to Greet AWS Users!

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. This is a test number four!
+          Edit <code>src/App.js</code> and save to reload. Now I am live on AWS! YaaaaaaaY!
         </p>
         <a
           className="App-link"
